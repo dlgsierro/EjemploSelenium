3057a0c98237f91fd39d7c3ab858a2480c1f829a
dlgsierro <gsierro@dl.cl>
2017-10-24 10:27:18 -0300

    Initial commit

---
+++ GoogleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PruebaSelenium")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4f4421b-1877-4001-8e86-06f9b85d6748")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GoogleTests/TestGoogleSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace PruebaSelenium
+{
+    [TestFixture]
+    public class SeleniumTests
+    {
+        [Test]
+        public void TestGoogleSearch()
+        {
+            IWebDriver driver = null;
+            try
+            {
+                driver = new FirefoxDriver();
+                driver.Navigate().GoToUrl("http://www.google.cl");
+                driver.FindElement(By.Name("q")).SendKeys("Pruebas Selenium");
+                driver.FindElement(By.Name("btnK")).Click();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            finally
+            {
+                if(driver != null)
+                {
+                    driver.Close();
+                    driver.Quit();
+                }
+            }
+        }
+    }
+}
